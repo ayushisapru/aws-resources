e97f8aaaa94173389951ca0335dd7b8d54979097
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-16 16:29:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ module "alb" {
 }
 
 output "alb_arn" {
-  value = module.my_alb.alb_arn
+  value = module.alb.alb_arn
 }
 
 output "alb_dns_name" {
-  value = module.my_alb.alb_dns_name
+  value = module.alb.alb_dns_name
 }
