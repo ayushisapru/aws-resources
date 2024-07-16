97a2c239844d7390b4b91832ed3533c77f86b2c6
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-16 13:07:40 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,44 @@
-variable "db_name" {
-  default = "exampledb"
+variable "region" {
+  description = "The AWS region to deploy the ALB"
+  type        = string
+  default     = "us-east-1"  
 }
 
-variable "db_user" {
-  default = "admin"
+variable "alb_name" {
+  description = "The name of the Application Load Balancer"
+  type        = string
+  default     = "my-alb"
 }
 
-variable "db_password" {
-  default = "password"
+variable "alb_internal" {
+  description = "Whether the load balancer is internal or external"
+  type        = bool
+  default     = false
 }
 
-variable "db_instance_class" {
-  default = "db.t2.micro"
+variable "security_groups" {
+  description = "A list of security group IDs to assign to the load balancer"
+  type        = list(string)
+  default     = ["sg-0123456789abcdef0"]  # update with security group IDs
 }
 
-variable "db_allocated_storage" {
-  default = 20
+variable "subnets" {
+  description = "A list of subnet IDs to attach to the load balancer"
+  type        = list(string)
+  default     = ["subnet-0123456789abcdef0", "subnet-abcdef0123456789"]  # update with subnet IDs
 }
 
-variable "db_engine" {
-  default = "postgresql"
+variable "enable_deletion_protection" {
+  description = "Whether deletion protection is enabled"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource"
+  type        = map(string)
+  default = {
+    Environment = "dev"
+    Project     = "my-project"
+  }
 }
