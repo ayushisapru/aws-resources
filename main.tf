5cba58ae516b73dc3fd626285a8802aa5bf3c4be
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-17 09:52:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+}
+
 resource "aws_security_group" "sg" {
   name_prefix = "alb_sg"
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.vpc.id
 
   ingress {
     from_port   = 80
@@ -22,8 +26,8 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_subnet" "subnet" {
-  count = length(var.subnet_ids)
-  vpc_id     = var.vpc_id
+  count = length(var.subnet_cidrs)
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = element(var.subnet_cidrs, count.index)
 }
 
