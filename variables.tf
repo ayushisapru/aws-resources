55258166648bdcbc4e17756158ef67c5f5051628
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-17 11:08:36 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,53 +1,29 @@
 variable "region" {
-  description = "The AWS region to create resources in."
+  description = "The AWS region to deploy the ALB"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-2"  
 }
-
 variable "alb_name" {
-  description = "The name of the ALB."
+  description = "The name of the Application Load Balancer"
   type        = string
+  default     = "my-alb"
 }
-
 variable "alb_internal" {
-  description = "Whether the ALB is internal."
+  description = "Whether the load balancer is internal or external"
   type        = bool
   default     = false
 }
 
-variable "vpc_id" {
-  description = "The VPC ID where resources will be created."
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs."
-  type        = list(string)
-}
-
-variable "subnet_cidrs" {
-  description = "List of subnet CIDR blocks."
-  type        = list(string)
-}
-
-variable "enable_deletion_protection" {
-  description = "Whether to enable deletion protection for the ALB."
-  type        = bool
-  default     = false
-}
-
-variable "tags" {
-  description = "Tags to be applied to resources."
-  type        = map(string)
-  default     = {}
-}
-
+/*
 variable "security_groups" {
   description = "A list of security group IDs to assign to the load balancer"
   type        = list(string)
+@@ -27,7 +27,7 @@ variable "subnets" {
+  type        = list(string)
+  default     = aws_subnet.subnet.id
 }
 
-variable "subnets" {
-  description = "A list of subnet IDs to attach to the load balancer"
-  type        = list(string)
-}
+*/
+variable "enable_deletion_protection" {
+  description = "Whether deletion protection is enabled"
+  type        = bool
