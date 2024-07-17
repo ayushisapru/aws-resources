918633231828c19f11c2592e8bf7109651f30fa8
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-17 11:33:00 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "alb_arn" {
   description = "The ARN of the ALB"
-  value       = module.alb.alb_arn
+  value       = module.alb.this_load_balancer_arn
 }
 
 output "alb_dns_name" {
   description = "The DNS name of the ALB"
-  value       = module.alb.alb_dns_name
+  value       = module.alb.this_load_balancer_dns_name
 }
 
 output "alb_zone_id" {
   description = "The Zone ID of the ALB"
-  value       = module.alb.alb_zone_id
+  value       = module.alb.this_load_balancer_zone_id
 }
