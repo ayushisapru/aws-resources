c9b8a7b419e2468d4109e32c066498bd90175606
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-17 09:39:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,31 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_security_group" "sg" {
+  name_prefix = "alb_sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  count = length(var.subnet_ids)
+  vpc_id     = var.vpc_id
+  cidr_block = element(var.subnet_cidrs, count.index)
+}
+
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "9.9.0"
@@ -9,7 +34,7 @@ module "alb" {
   name                     = var.alb_name
   internal                 = var.alb_internal
   security_groups          = [aws_security_group.sg.id]
-  subnets                  = aws_subnet.subnet.id
+  subnets                  = aws_subnet.subnet[*].id
   enable_deletion_protection = var.enable_deletion_protection
   tags                     = var.tags
 }
