095aa330e4c029583e50c21b449d66c62a4d1171
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-17 11:39:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
 provider "aws" {
   region = var.region
 }
-
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "9.9.0"
-
   name                     = var.alb_name
   internal                 = var.alb_internal
-  security_groups          = var.security_groups
-  subnets                  = var.subnets
+  security_groups          = [aws_security_group.sg.id]
+  subnets                  = var.subnets  
+  subnets                  = aws_subnet.subnet.id
   enable_deletion_protection = var.enable_deletion_protection
   tags                     = var.tags
+}
+output "alb_arn" {
+  value = module.alb.alb_arn
+}
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
 }
