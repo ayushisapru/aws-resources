a8ba4bc92158da77dea9678cb320926f4be2481d
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-30 14:00:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,8 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.7.0"
 
+  name                      = "my-autoscaling-group"
+  launch_configuration      = aws_launch_configuration.as_conf.name
   min_size                  = 0
   max_size                  = 2
   desired_capacity          = 1
@@ -49,4 +51,10 @@ data "aws_security_group" "default" {
     name   = "vpc-id"
     values = [data.aws_vpc.selected.id]
   }
+}
+
+resource "aws_launch_configuration" "as_conf" {
+  name          = "web_config"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 }
