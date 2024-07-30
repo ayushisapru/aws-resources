f22b8968cbfa7c558dc4250585a9749bc51cb0bb
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-30 13:38:37 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,35 +3,37 @@ variable "region" {
   type        = string
   default     = "us-east-2"  
 }
+
 variable "alb_name" {
   description = "The name of the Application Load Balancer"
   type        = string
   default     = "my-alb"
 }
+
 variable "alb_internal" {
   description = "Whether the load balancer is internal or external"
   type        = bool
   default     = false
 }
-
 /*
 variable "security_groups" {
   description = "A list of security group IDs to assign to the load balancer"
   type        = list(string)
   default     = [aws_security_group.sg.id]
 }
+
 variable "subnets" {
   description = "A list of subnet IDs to attach to the load balancer"
   type        = list(string)
   default     = aws_subnet.subnet.id
 }
-
 */
 variable "enable_deletion_protection" {
   description = "Whether deletion protection is enabled"
   type        = bool
   default     = true
 }
+
 variable "tags" {
   description = "A map of tags to assign to the resource"
   type        = map(string)
@@ -39,4 +41,9 @@ variable "tags" {
     Environment = "dev"
     Project     = "my-project"
   }
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
 }
