83e83f04a47cde4173020a05ca4d992f7efea6ae
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-31 09:20:58 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,3 @@
-output "alb_arn" {
-  description = "The ARN of the ALB"
-  value       = aws_lb.this.arn
-}
-
-output "alb_dns_name" {
-  description = "The DNS name of the ALB"
-  value       = aws_lb.this.dns_name
-}
-
 output "alb_zone_id" {
   description = "The zone_id of the ALB"
   value       = aws_lb.this.zone_id
